474fdbf75bc064aa3ff1e4f8a5193d6c248995a7
Broad <romulo.mgaudencio@gmail.com>
2022-08-18 20:13:21 -0300

    requisição ok

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import S from './Form.module.css'
 import image from '../../assets/image.svg'
 
 const Form = () => {
-  return (
+
+  const [cep, setCep] = useState('')
+  const [infos, setInfos] = useState('')
+
+  function handleCep(e) {
+      setCep(e.target.value)
+  }
+
+  const url = `https://viacep.com.br/ws/${cep}/json/`
+
+  async function handleReq(e) {
+    e.preventDefault()
+    const request = await fetch(url)
+    const json = await request.json()
+    setInfos(json)
+    console.log(json);
+  }
+
+  return (    
+
     <div className={S.container}>
         <img src={image}/>
         <form className={S.form}>
         <label>Digite o seu Cep</label>
-        <input type="text" placeholder='ex:18466-000'></input>
-        <button>Busca cep</button>
+        <input type="text" value={cep} placeholder='ex:18466-000' onChange={handleCep}></input>
+        <button onClick={handleReq}>Busca cep</button>
 
     </form>
     </div>
